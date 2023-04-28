66c5be8870b8b80447f970291cb8751ba9d8cdf3
CH V V GOPAL RAJU KALIDINDI <S559007@NWMISSOURI.EDU>
2023-04-27 20:52:30 -0500

    done

--- models/gas.js
+++ models/gas.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose")
 const gasSchema = mongoose.Schema({
-    gas_name: String,
-    quantity: Number,
-    types: String
+    gas_name: {
+        type: String,
+        
+    },
+    quantity: {
+        type:Number,
+        required:true,
+        min:1,
+        max:1000,
+        validate:{
+            validator:function(value){
+                return "quantity non negative and less than 1000 "
+            },
+            
+        }
+    
+    
+    },
+    types: {
+        type: String,
+        
+    }
 })
 module.exports = mongoose.model("gas",gasSchema)
--- routes/gas.js
+++ routes/gas.js
@@ -11,14 +11,21 @@
 var express = require('express');
 const gas_controlers= require('../controllers/gas');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET gass */
 router.get('/', gas_controlers.gases_view_all_Page );
 /* GET detail gas page */
-router.get('/detail', gas_controlers.gas_view_one_Page);
+router.get('/detail',secured, gas_controlers.gas_view_one_Page);
 /* GET create gas page */
-router.get('/create', gas_controlers.gas_create_Page);
+router.get('/create',secured, gas_controlers.gas_create_Page);
 /* GET create update page */
-router.get('/update', gas_controlers.gas_update_Page);
+router.get('/update',secured, gas_controlers.gas_update_Page);
 /* GET delete gas page */
-router.get('/delete', gas_controlers.gas_delete_Page);
+router.get('/delete',secured, gas_controlers.gas_delete_Page);
 module.exports = router;
